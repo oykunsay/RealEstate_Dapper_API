00ece02a6cd3b2e1bef533b8238d1e3dfb2738ea
Öykü Ünsay <67629217+oykunsay@users.noreply.github.com>
2024-07-11 19:32:19 +0300

    First commit

--- RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductDto.cs
+++ RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductDto.cs
@@ -2,12 +2,14 @@
 {
     public class ResultProductDto
     {
-        public int ProductID { get; set; }
-        public string Title { get; set; }
-        public decimal Price { get; set; }
-        public string City { get; set; }
-        public string District { get; set; }
-        public int ProductCategory { get; set; }
+        public int productID { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
+        public string coverimage { get; set; }
+        public string type { get; set; }
+        public string address { get; set; }
 
 
     }
--- RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDtos.cs
+++ RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDtos.cs
@@ -2,12 +2,15 @@
 {
     public class ResultProductDtos
     {
-            public int productID { get; set; }
-            public string title { get; set; }
-            public decimal price { get; set; }
-            public string city { get; set; }
-            public string district { get; set; }
-            public string categoryName { get; set; }
-        
+        public int productID { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
+        public string categoryName { get; set; }
+        public string coverimage { get; set; }
+        public string type { get; set; }
+        public string address { get; set; }
+
     }
 }
