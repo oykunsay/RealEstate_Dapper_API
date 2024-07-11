c8cf2b958ab7ac0e127c7a5f5d2d78ec0f50128d
Öykü Ünsay <67629217+oykunsay@users.noreply.github.com>
2024-07-11 16:10:29 +0300

    Add project files.

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultServicesComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultServicesComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
